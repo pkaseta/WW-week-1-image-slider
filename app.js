0654d7bd316664fa3717f5d81eb829d4d3a87a8e
Brian <thewardbunch@gmail.com>
2021-02-21 18:33:48 -0500

    added images and image array for demo

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const imageArray = []
+const imageArray = [
+  "./images/background1.jpg",
+  "./images/background2.jpg",
+  "./images/background3.jpg",
+  "./images/background4.jpg",
+  "./images/background5.jpg",
+];
