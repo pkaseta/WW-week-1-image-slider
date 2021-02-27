120184d54aa7483cd509fbdfd9c247ee26e9e2b2
Brian <thewardbunch@gmail.com>
2021-02-27 18:47:30 -0500

    Post Demo Code Push

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-const imageArray = [
-  "./images/background1.jpg",
-  "./images/background2.jpg",
-  "./images/background3.jpg",
-  "./images/background4.jpg",
-  "./images/background5.jpg",
-];
+const photoSlider = document.getElementById("photoSlider");
+const photoSliderImages = document.querySelectorAll("img");
+const prevButton = document.getElementById("prevImage");
+const nextButton = document.getElementById("nextImage");
+
+let counter = 1;
+
+photoSlider.style.transform = "translateX(" + -300 * counter + "px)";
+
+prevButton.addEventListener("click", function () {
+  if (counter === 0) {
+    return;
+  }
+  counter--;
+  photoSlider.style.transition = "transform 0.5s ease-in-out";
+  photoSlider.style.transform = "translateX(" + -300 * counter + "px)";
+  console.log("this works");
+  console.log(counter);
+});
+
+nextButton.addEventListener("click", function () {
+  if (counter === 6) {
+    return;
+  }
+  counter++;
+  photoSlider.style.transition = "transform 0.5s ease-in-out";
+  photoSlider.style.transform = "translateX(" + -300 * counter + "px)";
+  console.log("this works");
+  console.log(counter);
+});
+
+photoSlider.addEventListener("transitionend", function () {
+  if (photoSliderImages[counter].id === "lastImageCopy") {
+    photoSlider.style.transition = "none";
+    counter = photoSliderImages.length - 2;
+    photoSlider.style.transform = "translateX(" + -300 * counter + "px)";
+  }
+  if (photoSliderImages[counter].id === "firstImageCopy") {
+    photoSlider.style.transition = "none";
+    counter = photoSliderImages.length - counter;
+    photoSlider.style.transform = "translateX(" + -300 * counter + "px)";
+  }
+});
